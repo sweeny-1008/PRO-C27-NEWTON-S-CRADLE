9a05ef9190250ef11fd8f39c09f7b8ba0eb9b7d7
sweeny-1008 <73891429+sweeny-1008@users.noreply.github.com>
2020-12-02 17:59:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,49 +1,36 @@
-var  bullet  , wall, thickness;
-var speed,weight;
 
-function setup()
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
 {
-  createCanvas(1600,400);
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
   
-  //giving random values 
-  speed=random(30 , 52);
-  weight=random(400,1500);
-  thickness=random(22,83);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
   
-  //crearing car 
-  car=createSprite(50,200,50,50);
-  car.velocityX = speed;
-  car.shapeColour=colour(225);
-
-  //creating wall
-  wall=createSprite(1200,200,thickness,height/2);
-  wall.shapeColour=colour(80,80,80);
-  wall=createSprite(1500, 200, 60, height/2);
-  wall.velocityX = speed;
-
-  createSprite(400, 200, 50, 50);
-}
-
-function draw() 
-{
-  background(black);
-  if(wall.x-car.x<(car.width+wall.width)/2)
-  {
-    car.velocityX=speed;
-    var deformation = 0.5 * weight * speed /22509;
-  }
-  if(deformation>180)
-  {
-     car.shapeColour=colour(255,0,0);
-  }
-  if(deformation<180 && deformation>100)
-{
-    car.shapeColour=colour(230,230,0);
-}
-
-if(deformation<100)
-{
-  car.shapeColour=colour(0,255,0);
-}
   drawSprites();
+ 
 }
+
+
+
